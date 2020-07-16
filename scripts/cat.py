56c8e6eb304549ffee9f2550ada6e9cd690cf120
bear14 <bearlord14@gmail.com>
2020-07-16 15:26:22 +0200

    Init Obstacle avoid

--- scripts/cat.py
+++ scripts/cat.py
@@ -32,7 +32,7 @@ class Cat:
 
         rospy.Subscriber("cat/odom", Odometry, self.cat_odom_callback)
         rospy.Subscriber("mouse/odom", Odometry, self.mouse_odom_callback)
-        #rospy.Subscriber("cat/scan", LaserScan, self.laser_callback)
+        rospy.Subscriber("cat/scan", LaserScan, self.laser_callback)
 
         self.cat_publisher = rospy.Publisher("cat/cmd_vel", Twist, queue_size=10)
         rospy.spin()
@@ -42,7 +42,7 @@ class Cat:
         self.y_cat = odom.pose.pose.position.y
 
         quaternion = [odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z,
-                odom.pose.pose.orientation.w]
+                      odom.pose.pose.orientation.w]
         euler = euler_from_quaternion(quaternion)
         self.phi_cat = euler[2]
 
@@ -50,17 +50,15 @@ class Cat:
         self.x_mouse = odom.pose.pose.position.x
         self.y_mouse = odom.pose.pose.position.y
 
-
         self.calc_attraction_point()
         self.update_polar()
         self.homing()
 
-
-    # def laser_callback(self, current_laser_scan):
-    #     self.sensor_angles = np.arange(current_laser_scan.angle_min,
-    #                                    current_laser_scan.angle_max + current_laser_scan.angle_increment,
-    #                                    current_laser_scan.angle_increment)
-    #     self.sensor_ranges = np.array(current_laser_scan.ranges)
+    def laser_callback(self, current_laser_scan):
+        self.sensor_angles = np.arange(current_laser_scan.angle_min,
+                                       current_laser_scan.angle_max + current_laser_scan.angle_increment,
+                                       current_laser_scan.angle_increment)
+        self.sensor_ranges = np.array(current_laser_scan.ranges)
 
     def calc_attraction_point(self):
         if self.x_mouse and self.y_mouse:
@@ -68,14 +66,35 @@ class Cat:
             self.attraction_point_y = (self.y_mouse + self.y_cheese) * 0.5
 
     def update_polar(self):
-        # self.calc_attraction_point()
+        # make sure cat_odom_callback is at least called once
+        while self.x_cat is None and self.y_cat is None:
+            print("Wait for cat_odom_callback.")
 
         delta_x = self.attraction_point_x - self.x_cat
         delta_y = self.attraction_point_y - self.y_cat
 
-        #self.roh = m.sqrt(delta_x ** 2 + delta_y ** 2)  # Distanz zum ziel
+        self.roh = m.sqrt(delta_x ** 2 + delta_y ** 2)  # Distanz zum ziel
         self.alpha = m.atan2(delta_y, delta_x) - self.phi_cat  # Winkel zum ziel
 
+    def calculate_force(self):
+
+        force = np.zeros(2)
+
+        for i in range(len(self.sensor_ranges)):
+            if (self.sensor_ranges[i] < 0.7):
+                force[0] += -m.cos(self.sensor_angles[i]) * (0.7 - self.sensor_ranges[i])
+            if (self.sensor_ranges[i] < 0.8):
+                force[1] += -m.sin(self.sensor_angles[i]) * (0.8 - self.sensor_ranges[i])
+
+        force[0] = (force[0] / 50) + 0.8
+        if (force[0] > 0.8):
+            force[0] = 0.8
+
+        force[1] /= 24
+        if ((force[1] < 0.1) and (force[0] < 0.2)):
+            force[1] *= 2
+
+        return force
 
     def homing(self):
         k_rho_0 = 0.1
@@ -85,24 +104,31 @@ class Cat:
 
         if np.abs(self.alpha) < 0.3:
             if np.abs(self.rho) < 0.1:
-                #k_rho_0 = 1 * k_rho_0
+                # k_rho_0 = 1 * k_rho_0
                 k_alpha_0 = 1 * k_alpha_0
             else:
-                #k_rho_0 = 1 * k_rho_0
+                # k_rho_0 = 1 * k_rho_0
                 k_alpha_0 = 2 * k_alpha_0
         else:
             if np.abs(self.rho) < 0.1:
-                #k_rho_0 = 2 * k_rho_0
+                # k_rho_0 = 2 * k_rho_0
                 k_alpha_0 = 2 * k_alpha_0
             else:
-                #k_rho_0 = 0 * k_rho_0
+                # k_rho_0 = 0 * k_rho_0
                 k_alpha_0 = 4 * k_alpha_0
 
         out = Twist()
         out.linear.x = CONSTANT_CAT_SPEED  # k_rho_0 * self.rho # + force[0]
-        print(k_alpha_0 * self.alpha)
-        out.angular.z = k_alpha_0 * self.alpha  # + force[1]
+
+        force = self.calculate_force()
+
+        out.angular.z = k_alpha_0 * self.alpha + force[1] * 4
+        if out.angular.z > 0.8:
+            out.angular.z = 0.8
+        if out.angular.z < -0.8:
+            out.angular.z = -0.8
         self.cat_publisher.publish(out)
+
 
 if __name__ == '__main__':
     rospy.init_node('cat', anonymous=True)
