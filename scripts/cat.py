2083642d944a46c99c856efe8b2cdf3a3d38b992
N-Hart <haehnchenlord@yahoo.de>
2020-07-27 22:21:50 +0200

    set_state

--- scripts/cat.py
+++ scripts/cat.py
@@ -200,26 +200,28 @@ class Cat:
     def set_state(self):
 
         print(self.state)
-        dist_cat_mouse = self.distance(self.x_cat, self.y_cat, self.x_mouse, self.y_mouse)
-        dist_mc = self.distance(self.x_mouse,self.y_mouse,self.x_cheese,self.y_cheese)
-        dist_cc = self.distance(self.x_cat, self.y_cat, self.x_cheese, self.y_cheese)
-        if self.state == "cheese":
-            self.update_polar(self.x_cheese, self.y_cheese)
-            if self.distance(self.x_cheese,self.y_cheese,self.x_cat,self.y_cat) < GOAL_RADIUS:
-                self.state = "mid"
-        elif self.state == "mid":
-            self.calc_mid_point()
-            self.update_polar(self.attraction_point_x, self.attraction_point_y)
-            if self.distance(self.attraction_point_x,self.attraction_point_y,self.x_cat,self.y_cat) < GOAL_RADIUS:
-                self.state = "hunt"
-        elif self.state == "hunt":
-            self.calc_preditction_mouse()
-            self.update_polar(self.attraction_point_x, self.attraction_point_y)
-            if dist_cc > dist_mc:
-                self.state = "cheese"
         if (dist_cat_mouse < 0.9 and dist_mc > 2):
             self.update_polar(self.x_mouse, self.y_mouse)
             self.state = "hunt"
+        else:
+            dist_cat_mouse = self.distance(self.x_cat, self.y_cat, self.x_mouse, self.y_mouse)
+            dist_mc = self.distance(self.x_mouse,self.y_mouse,self.x_cheese,self.y_cheese)
+            dist_cc = self.distance(self.x_cat, self.y_cat, self.x_cheese, self.y_cheese)
+            if self.state == "cheese":
+                self.update_polar(self.x_cheese, self.y_cheese)
+                if self.distance(self.x_cheese,self.y_cheese,self.x_cat,self.y_cat) < GOAL_RADIUS:
+                    self.state = "mid"
+            elif self.state == "mid":
+                self.calc_mid_point()
+                self.update_polar(self.attraction_point_x, self.attraction_point_y)
+                if self.distance(self.attraction_point_x,self.attraction_point_y,self.x_cat,self.y_cat) < GOAL_RADIUS:
+                    self.state = "hunt"
+            elif self.state == "hunt":
+                self.calc_preditction_mouse()
+                self.update_polar(self.attraction_point_x, self.attraction_point_y)
+                if dist_cc > dist_mc:
+                    self.state = "cheese"
+        
 
         #
         # dist_mc = self.distance(self.x_mouse,self.y_mouse,self.x_cheese,self.y_cheese)
